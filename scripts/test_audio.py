46eda8e9ea1655c1fae06a41161ca69e872c685f
jtuinman <joriktuinman@hotmail.com>
2026-02-04 15:41:06 +0100

    wrong folder

--- scripts/test_audio.py
+++ scripts/test_audio.py
@@ -3,12 +3,12 @@ import time
 
 pygame.mixer.init()
 
-pygame.mixer.music.load("/home/pi/escaperoom/audio/state1.mp3")
+pygame.mixer.music.load("/home/pi/escape-sound-system/audio/state1.mp3")
 pygame.mixer.music.play(-1)   # loop
 
 time.sleep(5)
 
-hint = pygame.mixer.Sound("/home/pi/escaperoom/audio/hint1.mp3")
+hint = pygame.mixer.Sound("/home/pi/escape-sound-system/audio/hint1.mp3")
 hint.play()
 
 time.sleep(5)
